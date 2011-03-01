5bb044b40f2fcd83c928c70a5bbd7855f7703675
Jeremy Wall (zaphar) <jeremy@marzhillstudios.com>
2011-02-28 23:15:05 -0600

    doc and comments cleanup

--- pkg/html/transform/selector.go
+++ pkg/html/transform/selector.go
@@ -1,8 +1,6 @@
-/*
- Copyright 2010 Jeremy Wall (jeremy@marzhillstudios.com)
- Use of this source code is governed by the Artistic License 2.0.
- That License is included in the LICENSE file.
-*/
+// Copyright 2010 Jeremy Wall (jeremy@marzhillstudios.com)
+// Use of this source code is governed by the Artistic License 2.0.
+// That License is included in the LICENSE file.
 package transform
 
 import (
--- pkg/html/transform/transform.go
+++ pkg/html/transform/transform.go
@@ -1,17 +1,17 @@
+// Copyright 2010 Jeremy Wall (jeremy@marzhillstudios.com)
+// Use of this source code is governed by the Artistic License 2.0.
+// That License is included in the LICENSE file.
 /*
- Copyright 2010 Jeremy Wall (jeremy@marzhillstudios.com)
- Use of this source code is governed by the Artistic License 2.0.
- That License is included in the LICENSE file.
 
- The html transform package implements a html css selector and transformer.
+The html transform package implements a html css selector and transformer.
 
- An html doc can be inspected and queried using css selectors as well as
- transformed.
+An html doc can be inspected and queried using css selectors as well as
+transformed.
 
  	doc := NewDoc(str)
  	sel1 := NewSelector("div.foo")
  	sel2 := NewSelector("a")
-  t := NewTransform(doc)
+	t := NewTransform(doc)
  	newDoc := t.Apply(AppendChild, sel1)
   	.Apply(Replace, sel2)
   	.doc
