8d408e6690259ef29ab4538290ccd7697a9da300
Jeremy Wall (zaphar) <jeremy@marzhillstudios.com>
2011-10-31 22:01:56 -0500

    Fleshed out package documentation a little more.

--- h5/h5.go
+++ h5/h5.go
@@ -1,10 +1,16 @@
 /*
 	Package h5 implements an html5 parser for the go language.
 
-    import "h5"
  	p := h5.NewParser(rdr)
     err := p.Parse()
     tree := p.Tree()
+
+    ...
+    tree.Walk(func(n *Node) {
+       // do something with the node
+    })
+
+   tree2 := tree.Clone()
  */
 package h5
 
